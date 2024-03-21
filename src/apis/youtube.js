0b01c94982d901957bc54edd71dd376a6f34915d
Colson <colsoncalifornication@gmail.com>
2024-03-21 11:51:56 +0545

    add youtube api credential

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const KEY = '';
+const KEY = 'AIzaSyAgNXYZSUN1f1gvubeGiGqJFSBF47U2tJg';
 
 export default axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3',
